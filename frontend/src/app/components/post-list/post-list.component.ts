277d5541344089e3f0a6fd9e04f9fc884bef6375
hazeliscoding <hazel.granados@protonmail.com>
2025-12-15 14:17:08 -0600

    List, search, and delete posts...

--- frontend/src/app/components/post-list/post-list.component.ts
+++ frontend/src/app/components/post-list/post-list.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../../models/post.model';
+import { MockPostService } from '../../services/mock-post.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css',
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit {
+  posts: Post[] = [];
+  searchTerm: string = '';
 
+  constructor(private postService: MockPostService) {}
+
+  ngOnInit(): void {
+    this.loadPosts();
+  }
+
+  loadPosts() {
+    this.postService.getPosts(this.searchTerm).subscribe((posts) => (this.posts = posts));
+  }
+
+  onSearch() {
+    this.loadPosts();
+  }
+
+  deletePost(id: number) {
+    this.postService.deletePost(id).subscribe((success) => {
+      if (success) this.loadPosts();
+    });
+  }
 }
