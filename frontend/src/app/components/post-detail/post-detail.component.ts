abbe0b69852cb586f39d1061d7e83571f578f338
hazeliscoding <hazel.granados@protonmail.com>
2025-12-15 15:03:50 -0600

    We can now view post details :)

--- frontend/src/app/components/post-detail/post-detail.component.ts
+++ frontend/src/app/components/post-detail/post-detail.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../../models/post.model';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { MockPostService } from '../../services/mock-post.service';
 
 @Component({
   selector: 'app-post-detail',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './post-detail.component.html',
   styleUrl: './post-detail.component.css',
 })
-export class PostDetailComponent {
+export class PostDetailComponent implements OnInit {
+  post: Post | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private postService: MockPostService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const postId = +this.route.snapshot.paramMap.get('id')!;
+    this.postService.getPost(postId).subscribe(post => this.post = post);
+  }
 }
